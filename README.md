4130c9b5fffc4a2cbe340c3d7436f340617e3cd7
Mar0xy <marie@kaifa.ch>
2022-10-16 10:33:08 +0200

    initialize Next.js

---
+++ README.md
@@ -0,0 +1,5 @@
+# launcher.moe
+
+Here you can view the inner workings of [launcher.moe](https://launcher.moe) and see how the service works yourself while also being able to improve on top of it.
+
+(more to come here soon™)
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const IndexPage = () => {
+      return (
+        <>
+          <Head>
+            <title>Home</title>
+          </Head>
+          <h1>Empty</h1>
+        </>
+      );
+     
+};
+  
+export default IndexPage;
