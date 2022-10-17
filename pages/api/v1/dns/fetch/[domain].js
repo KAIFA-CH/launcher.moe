44b16bfede4a2362f31de45356cfe10d2803f0e5
Marie <marie@kaifa.ch>
2022-10-17 08:11:57 +0200

    Remove comment

--- pages/api/v1/dns/fetch/[domain].js
+++ pages/api/v1/dns/fetch/[domain].js
@@ -4,6 +4,5 @@ let cf = new Cloudflare({ token: process.env.CF_TOKEN });
 // @todo #fetch Make sure the person owns it in the DB and then fetch the record info for the provided domain
 
 export default async function handler(req, res) {
-    //console.log(await cf.dnsRecords.browse(process.env.CF_ZONE));
     res.end(`test`)
 }  
