a824bad5c9e06d3cdbd244a859178028fe4b455a
Mar0xy <marie@kaifa.ch>
2022-10-17 04:50:01 +0200

    Add ToDo

--- pages/api/v1/dns/fetch/[domain].js
+++ pages/api/v1/dns/fetch/[domain].js
@@ -1,7 +1,9 @@
 import Cloudflare from "cloudflare";
 let cf = new Cloudflare({ token: process.env.CF_TOKEN });
 
+// @todo #fetch Fetch record info from provided domain and make sure the person owns it in the DB
+
 export default async function handler(req, res) {
-    console.log(await cf.dnsRecords.browse(process.env.CF_ZONE));
+    //console.log(await cf.dnsRecords.browse(process.env.CF_ZONE));
     res.end(`test`)
 }  
