795a9aecdb99a69b245b6707d0bddca4c16ba811
Mar0xy <marie@kaifa.ch>
2022-10-22 13:18:53 +0200

    API: Fetch DNS

--- pages/api/v1/dns/fetch/[domain].js
+++ pages/api/v1/dns/fetch/[domain].js
@@ -1,8 +1,10 @@
-import Cloudflare from "cloudflare";
-let cf = new Cloudflare({ token: process.env.CF_TOKEN });
+import { withApiAuth } from '@supabase/auth-helpers-nextjs';
 
-// @todo #fetch Make sure the person owns it in the DB and then fetch the record info for the provided domain
-
-export default async function handler(req, res) {
-    res.end(`test`)
-}  
+export default withApiAuth(async function DomainHandler(req, res, supabaseServerClient) {
+    const { domain } = req.query;
+    const { data, error } = await supabaseServerClient.from('addresses').select('cname').eq('name', domain).single();
+    if (error && error.code == 'PGRST116')
+        res.json({code: error.code, msg: `Invalid Domain`});
+    else
+        res.json(data);
+});
